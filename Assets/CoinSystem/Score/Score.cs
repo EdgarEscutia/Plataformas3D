ada696cb1ddde9cc9c300d77dd18fc7d7f513346
EdgarEscutia <edgarescutiaperal@gmail.com>
2025-06-03 11:04:34 +0200

    primera parte acabada

--- Assets/CoinSystem/Score/Score.cs
+++ Assets/CoinSystem/Score/Score.cs
@@ -12,12 +12,16 @@ public class Score_Canvas : MonoBehaviour
     [SerializeField] GameObject Puerta2;
     [SerializeField] GameObject Puerta3;
     [SerializeField] GameObject Puerta4;
+    [SerializeField] GameObject Puerta5;
+    [SerializeField] GameObject Puerta6;
 
 
     [SerializeField] int puerta1;
     [SerializeField] int puerta2;
     [SerializeField] int puerta3;
     [SerializeField] int puerta4;
+    [SerializeField] int puerta5;
+    [SerializeField] int puerta6;
 
     // Start is called before the first frame update
     void Start()
@@ -33,24 +37,30 @@ public class Score_Canvas : MonoBehaviour
         score.text = $"{Coin.count} / {Coin.maxCount}";
         
         if(Coin.count == puerta1)
-        {
-            Puerta1.SetActive(false);
-        }
+        { Puerta1.SetActive(false);}
+           
+        
         if (Coin.count == puerta2)
-        {
-            Puerta2.SetActive(false);
-        }
+        { Puerta2.SetActive(false); }
+           
+       
         if (Coin.count == puerta3)
-        {
-            Puerta3.SetActive(false);
-        }
+        {Puerta3.SetActive(false); }
+            
+       
         if (Coin.count == puerta4)
-        {
-            Puerta4.SetActive(false);
-        }
+        { Puerta4.SetActive(false);}
+
+        if (Coin.count == puerta5)
+        { Puerta5.SetActive(false); }
+
+        if (Coin.count == puerta6)
+        { Puerta6.SetActive(false); }
+
+
         if (Coin.count == 0)
-        {
-            victoryCanvas.gameObject.SetActive(true);
-        }
+        {victoryCanvas.gameObject.SetActive(true);}
+            
+        
     }
 }
